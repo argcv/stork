b047287e7bf4ba34253cb44c8d92b6acb0352e46
Yu Jing <yuikns@users.noreply.github.com>
2020-08-05 20:11:48 +0800

    [Fix] busy waiting issue

--- mtx/wg_state.go
+++ mtx/wg_state.go
@@ -1,25 +1,35 @@
 package mtx
 
 import (
-	"github.com/argcv/stork/log"
-	"runtime"
+	"sync"
 	"sync/atomic"
+
+	"github.com/argcv/stork/log"
 )
 
 // Compare with waiting group
 // it will return current state
 // aka **How many workers are still working**
-type WaitGroupWithState struct {
-	st int64
+type WaitGroupWithState interface {
+	Add(delta int64) int64
+	Done() int64
+	State() int64
+	Wait()
 }
 
-func NewWaitGroupWithState() *WaitGroupWithState {
-	return &WaitGroupWithState{
+type waitGroupWithStateImpl struct {
+	st int64
+	cv *sync.Cond
+}
+
+func NewWaitGroupWithState() WaitGroupWithState {
+	return &waitGroupWithStateImpl{
 		st: 0,
+		cv: sync.NewCond(&sync.Mutex{}),
 	}
 }
 
-func (wg *WaitGroupWithState) Add(delta int64) int64 {
+func (wg *waitGroupWithStateImpl) Add(delta int64) int64 {
 	newSt := atomic.AddInt64(&(wg.st), delta)
 	if newSt < 0 {
 		log.Fatalf("ERROR: status is lower than 0!!! (%v)", newSt)
@@ -28,16 +38,20 @@ func (wg *WaitGroupWithState) Add(delta int64) int64 {
 }
 
 // minus one, return current value
-func (wg *WaitGroupWithState) Done() int64 {
-	return wg.Add(-1)
+func (wg *waitGroupWithStateImpl) Done() int64 {
+	newSt := wg.Add(-1)
+	wg.cv.Broadcast()
+	return newSt
 }
 
-func (wg *WaitGroupWithState) State() int64 {
+func (wg *waitGroupWithStateImpl) State() int64 {
 	return atomic.LoadInt64(&(wg.st))
 }
 
-func (wg *WaitGroupWithState) Wait() {
+func (wg *waitGroupWithStateImpl) Wait() {
 	for wg.State() > 0 {
-		runtime.Gosched()
+		wg.cv.L.Lock()
+		wg.cv.Wait()
+		wg.cv.L.Unlock()
 	}
 }
